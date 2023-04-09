3008442ac96bde2afb48738b399b84c877e91517
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-09 21:20:31 +0300

    make webhook secret static generated based on deta app data

--- BotMicro/bot/factory.py
+++ BotMicro/bot/factory.py
@@ -1,7 +1,5 @@
 import asyncio
-from os import getenv, environ
-from string import ascii_letters, digits
-from random import choices
+from os import getenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
@@ -13,11 +11,15 @@ from bot.middlewares.logging import LoggingMiddleware
 from bot.middlewares.callback_message import CallbackMessageMiddleware
 
 
+def get_webhook_secret() -> str:
+    return getenv('DETA_SPACE_APP_MICRO_NAME', '') + getenv('DETA_PROJECT_KEY', '')[:4]
+
+
 def create_bot(token: str) -> tuple[Bot, str]:
     bot = Bot(token, parse_mode='HTML')
 
     webhook_url = getenv('DETA_SPACE_APP_HOSTNAME', '') + '/webhook'
-    webhook_secret = ''.join(choices(ascii_letters + digits, k=32))
+    webhook_secret = get_webhook_secret()
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(bot.set_webhook(url=webhook_url, secret_token=webhook_secret))
