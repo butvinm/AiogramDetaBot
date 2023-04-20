6f915a9c71fcda9ecee850cbc2c5b66b4532a6a7
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-20 05:44:01 +0300

    add support of new python micros running on deta | refactor set webhook

--- BotMicro/main.py
+++ BotMicro/main.py
@@ -5,20 +5,18 @@ from deta import Deta
 from bot.factory import create_bot, create_dispatcher
 from web.factory import create_app
 
-
 BOT_TOKEN = getenv('BOT_TOKEN')
 assert BOT_TOKEN
 
 
 deta = Deta()
 
-bot, webhook_secret = create_bot(BOT_TOKEN)
+bot = create_bot(BOT_TOKEN)
 dispatcher = create_dispatcher(deta)
 
 
 app = create_app(
     deta,
     bot,
-    dispatcher,
-    webhook_secret
+    dispatcher
 )
